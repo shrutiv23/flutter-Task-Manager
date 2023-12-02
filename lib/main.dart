114d162685175a9100fb29f38e82d47004b2b370
Sanskrati Gawande <48903464+sanskrati3@users.noreply.github.com>
2023-12-02 15:24:04 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'ToDo App',
+      title: 'Task Management',
       home: Home(),
     );
   }
