8dbce8deb1705685dcf96965d4125621ed23e03e
Nirali Kansara <31383909+niralikansara@users.noreply.github.com>
2023-12-02 15:26:16 +0530

    Update colors.dart

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
-const Color tdRed = Color(0xFFDA4040);
-const Color tdBlue = Color(0xFF5F52EE);
-
+const Color tdRed = Color.fromARGB(255, 20, 101, 182);
+const Color tdBlue = Color.fromARGB(255, 238, 82, 82);
+ 
 const Color tdBlack = Color(0xFF3A3A3A);
 const Color tdGrey = Color(0xFF717171);
-
+ 
 const Color tdBGColor = Color(0xFFEEEFF5);
+ 
